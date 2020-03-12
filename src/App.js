0574a2db118fec0bb5bee6a7ec611c05742f4950
Edson <edson_csl@hotmail.com>
2020-03-12 18:12:41 -0300

    base project and create component table

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Table from './Table'
+class App extends React.Component {
+  state = {
+     autores: [
+      {
+        nome: 'Loiane',
+        livro:'Estrutura de dados e algoritmo js',
+        preco:65.00
+      },
+      {
+        nome: 'Paulo',
+        livro:'React',
+        preco:165.00
+        
+      },
+      {
+        nome: 'Flavio Almeida',
+        livro:'Cangaceiro js',
+        preco:60.00
+      }
+    ]
+  }
+  removeAutor = index => {
+    const { autores } = this.state;
+   
+    this.setState(
+      {
+        autores: autores.filter( ( autor, position ) => { 
+          return index !== position;
+        })
+      }
+    )
+  }
+ 
+ render(){
+    return (
+      <div className="App">
+        <Table autores={ this.state.autores } removeAutor={this.removeAutor}/>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      </div>
+    );
+ }
 }
 
 export default App;
