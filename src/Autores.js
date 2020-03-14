32b61a345900fc4cb66d3424b23d7b0061a90102
Edson <edson_csl@hotmail.com>
2020-03-14 16:08:39 -0300

    Create new components about, authors, NotFound, Books

---
+++ src/Autores.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import Header from './Header';
+
+
+const Autores = () => {
+  return (
+    <Fragment>
+      <Header></Header>
+      <h1>Autores</h1>
+
+    </Fragment>
+  )
+
+}
+
+export default Autores
---
+++ src/Sobre.js
@@ -0,0 +1,16 @@
+import React, {Fragment} from 'react';
+import Header from './Header';
+
+
+const Sobre = () => {
+  return (
+    <Fragment>
+      <Header></Header>
+      <h1>Sobre nós</h1>
+
+    </Fragment>
+  )
+
+}
+
+export default Sobre
